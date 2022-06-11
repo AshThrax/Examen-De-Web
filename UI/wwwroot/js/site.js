db6be918d1a1c9b9865d8810d0a51dba66f598b3
AshThrax <doom96.oc@gmail.com>
2022-06-12 01:04:56 +0200

    youtube

--- UI/wwwroot/js/site.js
+++ UI/wwwroot/js/site.js
@@ -2,80 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-gapi.load("client", loadClient);
 
-
-function loadClient()
+var api ="https://www.googleapis.com/youtube/v3/search?key="
+$(document).ready(function ()
 {
-    gapi.client.setApiKey("AIzaSyBQn4jTntWHt5QeZJ4xBORFQkKg9jJorBA")
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function () { console.log("GAPI client loaded for API"); },
-            function (err) { console.error("Error loading GAPI client for API", err); });
-}
-const ytForm = document.getElementById('yt-form');
-const keywordInput = document.getElementById('keyword-input');
-const maxresultInput = document.getElementById('maxresult-input');
-const orderInput = document.getElementById('order-input');
-const videoList = document.getElementById('videoListContainer');
-var pageToken = '';
 
-ytForm.addEventListener('submit', e => {
-    e.preventDefault();
-    execute();
-});
-
-function paginate(e, obj) {
-    e.preventDefault();
-    pageToken = obj.getAttribute('data-id');
-    execute();
-}
-
-// Make sure the client is loaded before calling this method.
-function execute() {
-    const searchString = keywordInput.value;
-    const maxresult = maxresultInput.value;
-    const orderby = orderInput.value;
-
-    var arr_search = {
-        "part": 'snippet',
-        "type": 'video',
-        "order": orderby,
-        "maxResults": maxresult,
-        "q": searchString
-    };
-
-    if (pageToken != '') {
-        arr_search.pageToken = pageToken;
+    var key = "AIzaSyBQn4jTntWHt5QeZJ4xBORFQkKg9jJorBA"
+    var Video = ""
+    var Videos=$("#videos")
+    $("form").submit(function (event)
+    {
+        event.preventDefault()
+        var search=$("#search").val()
+        videoSearch(key,search,10)
     }
+        )
 
-    return gapi.client.youtube.search.list(arr_search)
-        .then(function (response) {
-            // Handle the results here (response.result has the parsed body).
-            const listItems = response.result.items;
-            if (listItems) {
-                let output = '<h4>Videos</h4><ul>';
+    function videosearch(key, search, maxresult) {
+        $.get("https://www.googleapis.com/youtube/v3/search?key="key + "&type=video&part=snippet&maxResult=" + maxresult + "&q" + search, function (data)
+        {
+            console.log(data)
+            data.items.forEach( item => {
+                Video ='<iframe width="300" height="315" src="htto://www.youtube.com/embed/ ${item.id.videoId}" frameborder="0" allowfullscreen></iframe>'
 
-                listItems.forEach(item => {
-                    const videoId = item.id.videoId;
-                    const videoTitle = item.snippet.title;
-                    output += `
-                    <li><a data-fancybox href="https://www.youtube.com/watch?v=${videoId}"><img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" /></a><p>${videoTitle}</p></li>
-                `;
-                });
-                output += '</ul>';
-
-                if (response.result.prevPageToken) {
-                    output += `<br><a class="paginate" href="#" data-id="${response.result.prevPageToken}" onclick="paginate(event, this)">Prev</a>`;
-                }
-
-                if (response.result.nextPageToken) {
-                    output += `<a href="#" class="paginate" data-id="${response.result.nextPageToken}" onclick="paginate(event, this)">Next</a>`;
-                }
-
-                // Output list
-                videoList.innerHTML = output;
-            }
-        },
-            function (err) { console.error("Execute error", err); });
+                   
+                $("#videos").append(Video)
+            });
+        })
+    }
 }
+    )
+
    
