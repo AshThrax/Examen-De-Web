cfb862b6f011283986e1119b72b37a976ca600c0
AshThrax <doom96.oc@gmail.com>
2022-06-18 01:57:33 +0200

    commit

---
+++ infrastructure/Persistence/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure.Persistence
+{
+    public class SeedData
+    {
+        public static void main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services=scope.ServiceProvider;
+                try
+                {
+                    SeedDataBase.Initialize(services);
+                }
+                catch (Exception ex)
+
+                {
+                    var logger = services.getRequiredService<ILogger<Program>>();
+                    logger.logError(ex, "an errir occured seeding the Db");
+                }
+            }
+            host.Run();
+        }
+    }
+}
---
+++ infrastructure/Persistence/SeedDataBase.cs
@@ -0,0 +1,60 @@
+﻿using Api.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure.Persistence
+{
+    public static class SeedDataBase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            if (context.Films.Any())
+            {
+                return;
+            }
+            context.Films.AddRange(
+                new Domain.Entities.Film
+                {
+                    Id=1,
+                    Titre="the Man",
+                    Description="L'histoire d'un homme qui réussi a surpasser ses peurs",
+                    Date=DateTime.Now,
+                    Genre="Drame"
+
+                },
+                 new Domain.Entities.Film
+                 {
+                     Id = 1,
+                     Titre = "the M",
+                     Description = "tout commence le jours ou david trouvas un medaillons sacré qui le transporta vers un monde effroyable, pourra t'il y echapper",
+                     Date = DateTime.Now,
+                     Genre = "Horreur"
+                 },
+                  new Domain.Entities.Film 
+                  {
+                      Id = 1,
+                      Titre = "Die hard 12",
+                      Description = "John Mclane reprends du service ,et dois sauver le president ainsi que l'amérique",
+                      Date = DateTime.Now,
+                      Genre = "Drame"
+                  },
+                   new Domain.Entities.Film 
+                   {
+                       Id = 1,
+                       Titre = "Rambo 8",
+                       Description = "le gouvernement cherche le lieutenant john rambo pour une dernière mission a Wuhan y suvrira t'il?",
+                       Date = DateTime.Now,
+                       Genre = "Drame"
+                   }
+                );
+            context.SaveChanges();
+        }
+    }
+}
