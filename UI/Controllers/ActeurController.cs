98788a61cbc0bea815205172539f93ae08f141c8
AshThrax <doom96.oc@gmail.com>
2022-06-18 00:36:33 +0200

    change

---
+++ UI/Controllers/ActeurController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UI.Client;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class ActeurController : Controller
+    {
+        private readonly IActeurClient acteurClient;
+        public IActionResult Index()
+        {
+            var task = acteurClient.GetallAsync();
+            return View();
+        }
+        public IActionResult AddActeur()
+        {
+
+            return View();
+        }
+        public IActionResult AddActeur([FromBody] ActeurViewModel model, int filmid)
+        {
+            acteurClient.PostAsync(model);
+
+            return RedirectToAction("Index", "Film");
+        }
+
+        public IActionResult Delete([FromBody] int id) 
+        {
+            acteurClient( id);
+        }
+
+    }
+}
